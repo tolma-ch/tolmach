8c4e55e2a6d4f18b5c85ee0f98755e1fd25d369a
Dmitry Chumak <mega.venik@gmail.com>
2013-12-02 02:53:22 +0900

    tested invites

--- tolmach/templatetags/tolmach_tags.py
+++ tolmach/templatetags/tolmach_tags.py
@@ -3,6 +3,7 @@ from django import template
 register = template.Library()
 
 from tolmach.models import UserMeta, Messages
+from translations.models import Project
 
 @register.inclusion_tag('main/navbar-logged-in.html', takes_context=True)
 def login_navbar(context):
@@ -17,10 +18,12 @@ def login_navbar(context):
         meta = UserMeta.objects.get(user=user)
 
     invites = meta.invited_to.split(',') if not meta.invited_to == "" else []
+    project_invites = Project.objects.filter(id__in=invites)
     messages = []
 
     return {
-        'invites': invites,
+        'invites': project_invites,
+        'invites_num': len(project_invites),
         'messages': messages,
         'username': user.username
         }
