f4004c2070c7a61b428f0f4ad22fbe15f843ef03
Alexander Kuzmin <lonelind@vigo.su>
2014-05-13 09:24:24 +0400

    Little fixes

--- components/main/scripts/_.js
+++ components/main/scripts/_.js
@@ -1,6 +1,6 @@
 (function() {
   window.tolmach = {
-    version: "0.0.2-6"
+    version: "0.0.2-9"
   };
 
 }).call(this);
--- media/js/all.js
+++ media/js/all.js
@@ -5,7 +5,7 @@
 
 (function() {
   window.tolmach = {
-    version: "0.0.2-6"
+    version: "0.0.2-9"
   };
 
 }).call(this);
