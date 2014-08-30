fe3a07948f188453e5b443428d6b84fba1724f23
Alexander Kuzmin <lonelind@vigo.su>
2014-08-30 22:16:26 +0400

    [+] Logged-in state

--- media/js/all.js
+++ media/js/all.js
@@ -10,7 +10,7 @@
 
 (function() {
   window.Tolmach = {
-    version: "0.0.2-95"
+    version: "0.0.2-379"
   };
 
 }).call(this);
@@ -19,6 +19,16 @@
 /*
 TODO: make something here
  */
+
+(function() {
+
+
+}).call(this);
+
+(function() {
+
+
+}).call(this);
 
 (function() {
 
