3e8c6fb59e8fcbd4d136640185ea840d55af9c0d
Dmitry Chumak <mega.venik@gmail.com>
2015-04-26 13:15:59 +0800

    i18n first steps. Need to spread it all over the project now and pass to translators

--- tolmach/settings.py
+++ tolmach/settings.py
@@ -113,6 +113,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'social.apps.django_app.context_processors.login_redirect',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.i18n',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -121,6 +122,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
