5da1eedfd36d5c736fff40dff4dd3c8ee4321182
Evgeniy Finkelstein <exsmund@gmail.com>
2015-07-22 10:21:37 +0800

    fix participants list

--- translations/utils_ajax.py
+++ translations/utils_ajax.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from tolmach.models import UserMeta
 
 
 def entry_to_json(entry):
@@ -17,4 +18,15 @@ def translation_to_json(translation):
         },
         'isApproved': translation.is_approved,
         'vote': translation.vote
+    }
+
+
+def user_to_json(user):
+    username = '%s %s (%s)' % (user.first_name, user.last_name, user.username)
+    user_meta = UserMeta.objects.get(user=user)
+    avatar = "%s" % user_meta.avatar if user_meta.avatar else "avatar/default.png"
+    return {
+        'id': user.id,
+        'name': username,
+        'avatar': avatar
     }
