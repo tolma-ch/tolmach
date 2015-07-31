bf4bbfa95961d4d58a05676b25d9491bda2030af
Dmitry Chumak <mega.venik@gmail.com>
2015-07-31 18:12:23 +0800

    preloading data

--- tolmach/views.py
+++ tolmach/views.py
@@ -42,60 +42,6 @@ def index(request):
                         message=message
                     )
                     new_message.save()
-                try:
-                    test_project = Project.objects.get(name="Alfa test project", manager=request.user)
-                    print "Project is okay, nothing to do"
-                except Project.DoesNotExist:
-                    print "Woow, no preset project, need to populate!"
-                    test_project = Project(name="Alfa test project",
-                                           is_private=True,
-                                           manager=request.user,
-                                           description="123",
-                                           )
-                    test_project.save()
-                    print "Project populated!"
-                test_project = Project.objects.get(name="Alfa test project", manager=request.user)
-
-                # English glossary
-                en_glossary = utils.copy_glossary("English glossary", 2, test_project, request.user)
-                en_tmdb = utils.copy_tmdb("English->Russian translation memory", 2, test_project, request.user)
-                utils.copy_text("The Hobbit", 8, test_project, en_glossary, en_tmdb)
-
-                # Spanish glossary
-                es_glossary = utils.copy_glossary("Spanish glossary", 3, test_project, request.user)
-                es_tmdb = utils.copy_tmdb("Spanish->Russian translation memory", 3, test_project, request.user)
-                utils.copy_text("Riquete el de copete", 9, test_project, es_glossary, es_tmdb)
-
-                # Italian glossary
-                it_glossary = utils.copy_glossary("Italian glossary", 4, test_project, request.user)
-                it_tmdb = utils.copy_tmdb("Italian->Russian translation memory", 4, test_project, request.user)
-                utils.copy_text("La solitudine dei numeri primi", 10, test_project, it_glossary, it_tmdb)
-
-                # Japanese
-                ja_glossary = utils.copy_glossary("Japanese glossary", 5, test_project, request.user)
-                ja_tmdb = utils.copy_tmdb("Japanese->Russian translation memory", 5, test_project, request.user)
-                utils.copy_text("日本の山水画展", 11, test_project, ja_glossary, ja_tmdb)
-
-                # French
-                fr_glossary = utils.copy_glossary("French glossary", 6, test_project, request.user)
-                fr_tmdb = utils.copy_tmdb("French->Russian translation memory", 6, test_project, request.user)
-                utils.copy_text("Que sait-on sur le MERS-Coronavirus?", 12, test_project, fr_glossary, fr_tmdb)
-
-                # Korean glossary
-                ko_glossary = utils.copy_glossary("Korean glossary", 7, test_project, request.user)
-                ko_tmdb = utils.copy_tmdb("Korean->Russian translation memory", 7, test_project, request.user)
-                utils.copy_text("국회 대표단 러시아 바이칼 경제 포럼 참석하여 의원외교 펼쳐", 13, test_project, ko_glossary, ko_tmdb)
-
-                # German glossary
-                de_glossary = utils.copy_glossary("German glossary", 8, test_project, request.user)
-                de_tmdb = utils.copy_tmdb("German->Russian translation memory", 8, test_project, request.user)
-                utils.copy_text("Das brot der frühen jahre", 14, test_project, de_glossary, de_tmdb)
-
-                # Chinese
-                zh_glossary = utils.copy_glossary("Chinese glossary", 9, test_project, request.user)
-                zh_tmdb = utils.copy_tmdb("Chinese->Russian translation memory", 9, test_project, request.user)
-                utils.copy_text("松树镇", 15, test_project, zh_glossary, zh_tmdb)
-
 
         first_name = request.user.first_name
         last_name = request.user.last_name
@@ -131,6 +77,64 @@ def index(request):
         }
         template = 'main/main.html'
     return render_to_response(template, data, RequestContext(request))
+
+
+def populate_test_data(request):
+    try:
+        test_project = Project.objects.get(name="Alfa test project", manager=request.user)
+        print "Project is okay, nothing to do"
+    except Project.DoesNotExist:
+        print "Woow, no preset project, need to populate!"
+        test_project = Project(name="Alfa test project",
+                               is_private=True,
+                               manager=request.user,
+                               description="123",
+                               )
+        test_project.save()
+        print "Project populated!"
+        test_project = Project.objects.get(name="Alfa test project", manager=request.user)
+
+        # English glossary
+        en_glossary = utils.copy_glossary("English glossary", 2, test_project, request.user)
+        en_tmdb = utils.copy_tmdb("English->Russian translation memory", 2, test_project, request.user)
+        utils.copy_text("The Hobbit", 8, test_project, en_glossary, en_tmdb)
+
+        # Spanish glossary
+        es_glossary = utils.copy_glossary("Spanish glossary", 3, test_project, request.user)
+        es_tmdb = utils.copy_tmdb("Spanish->Russian translation memory", 3, test_project, request.user)
+        utils.copy_text("Riquete el de copete", 9, test_project, es_glossary, es_tmdb)
+
+        # Italian glossary
+        it_glossary = utils.copy_glossary("Italian glossary", 4, test_project, request.user)
+        it_tmdb = utils.copy_tmdb("Italian->Russian translation memory", 4, test_project, request.user)
+        utils.copy_text("La solitudine dei numeri primi", 10, test_project, it_glossary, it_tmdb)
+
+        # Japanese
+        ja_glossary = utils.copy_glossary("Japanese glossary", 5, test_project, request.user)
+        ja_tmdb = utils.copy_tmdb("Japanese->Russian translation memory", 5, test_project, request.user)
+        utils.copy_text("日本の山水画展", 11, test_project, ja_glossary, ja_tmdb)
+
+        # French
+        fr_glossary = utils.copy_glossary("French glossary", 6, test_project, request.user)
+        fr_tmdb = utils.copy_tmdb("French->Russian translation memory", 6, test_project, request.user)
+        utils.copy_text("Que sait-on sur le MERS-Coronavirus?", 12, test_project, fr_glossary, fr_tmdb)
+
+        # Korean glossary
+        ko_glossary = utils.copy_glossary("Korean glossary", 7, test_project, request.user)
+        ko_tmdb = utils.copy_tmdb("Korean->Russian translation memory", 7, test_project, request.user)
+        utils.copy_text("국회 대표단 러시아 바이칼 경제 포럼 참석하여 의원외교 펼쳐", 13, test_project, ko_glossary, ko_tmdb)
+
+        # German glossary
+        de_glossary = utils.copy_glossary("German glossary", 8, test_project, request.user)
+        de_tmdb = utils.copy_tmdb("German->Russian translation memory", 8, test_project, request.user)
+        utils.copy_text("Das brot der frühen jahre", 14, test_project, de_glossary, de_tmdb)
+
+        # Chinese
+        zh_glossary = utils.copy_glossary("Chinese glossary", 9, test_project, request.user)
+        zh_tmdb = utils.copy_tmdb("Chinese->Russian translation memory", 9, test_project, request.user)
+        utils.copy_text("松树镇", 15, test_project, zh_glossary, zh_tmdb)
+
+    return HttpResponseRedirect('/')
 
 
 def user_page(request, user_id):
