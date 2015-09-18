daff3163bb7bca36975b256c2c6c544a2ac67c33
Dmitry Chumak <mega.venik@gmail.com>
2015-09-18 14:06:18 +0800

    text progressbar fixes

--- translations/models.py
+++ translations/models.py
@@ -150,7 +150,7 @@ class TextTranslation(models.Model):
         entries_approved/(entries_total/100.0)
         """
         entries_total = TextEntry.objects.filter(text=self.text, parent_entry=None).count()
-        entries_translated = TextEntry.objects.filter(~Q(parent_entry=None), text=self.text, translation=self).count()
+        entries_translated = TextEntry.objects.filter(~Q(parent_entry=None), text=self.text, translation=self).values('parent_entry').distinct().count()
         entries_approved = TextEntry.objects.filter(text=self.text, translation=self, is_approved=True).count()
         print self.text.title
         print [entries_total, entries_translated, entries_approved]
