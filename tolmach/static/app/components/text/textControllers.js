44cfa12ed1e848e61b50e0e3b0dae8ef9c3ff480
exsmund <exsmund@gmail.com>
2015-12-14 16:37:38 +0800

    fix auto approve

--- tolmach/static/app/components/text/textControllers.js
+++ tolmach/static/app/components/text/textControllers.js
@@ -119,7 +119,7 @@
                         $scope.activeEntry = entry;
                         if (entry.mode === 1) {
                             setTimeout(function () {
-                                $('#entry-' + entry.idInText).find('textarea').focus();
+                                $('#entry-suggestion-' + entry.id).focus();
                             }, 10);
                         }
                     }
