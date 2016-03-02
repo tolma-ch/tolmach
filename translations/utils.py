6d11b43d4c66a9ef4ecf77d1d318c19af7058617
Dmitry Chumak <mega.venik@gmail.com>
2016-03-03 01:39:09 +0800

    [fix] Glossaries upload and parsing fixed

--- translations/utils.py
+++ translations/utils.py
@@ -146,7 +146,16 @@ def split_text(line_to_translate, lang='en', pattern="", num_in_text=1):
 
 
 def parse_glossary(file_on_disk, filetype):
+    def decode(s, encodings=('ascii', 'utf-8', 'cp1251')):
+        for encoding in encodings:
+            try:
+                return s.decode(encoding)
+            except UnicodeDecodeError:
+                pass
+        return s.decode('ascii', 'ignore')
+
     array = []
+
     with open(file_on_disk, 'r') as file_to_show:
         # открываем файл
         for line in file_to_show:
@@ -158,7 +167,7 @@ def parse_glossary(file_on_disk, filetype):
                     print line.decode('utf-8').rstrip().split('\t', 1)
                     array.append(line.decode('utf-8').rstrip().split('\t', 1))
                 elif filetype == "text/csv":
-                    array.append(line.decode('utf-8').rstrip().split(',', 1))
+                    array.append(decode(line).rstrip().split(',', 1))
 
     os.remove(file_on_disk)
 
