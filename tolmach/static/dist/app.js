5eb87ad8f7d873e2209f9f084cffcfe09c1ea58d
exsmund <exsmund@gmail.com>
2016-04-03 17:47:34 +0800

    fix help and landing

--- tolmach/static/dist/app.js
+++ tolmach/static/dist/app.js
@@ -2013,6 +2013,7 @@
                 $scope.currentBlock = $scope.helpBlocks.shift();
                 $scope.clickBlock = false;
                 $scope.leftAlign = false;
+                $scope.position = 'top';
                 $scope.hasNext = !!$scope.helpBlocks.length;
                 if ($scope.currentBlock) {
                     $scope.helpText = $($scope.currentBlock).attr('help-text');
