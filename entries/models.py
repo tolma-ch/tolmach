991c1d947e9e800cc26f47a7dc488d0008ea20bc
Dmitry Chumak <mega.venik@gmail.com>
2016-04-10 15:58:43 +0800

    [imp] Gettext plural forms formulas are now in our db

--- entries/models.py
+++ entries/models.py
@@ -6,6 +6,7 @@ class Language(models.Model):
     name = models.CharField(max_length=64)
     code = models.CharField(max_length=3)
     code_639_3 = models.CharField(max_length=3)
+    plural_forms = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
