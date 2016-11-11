8cf874d51d97113f5dcb86f82701cea72a62f383
Dmitry Chumak <mega.venik@gmail.com>
2016-11-11 23:29:09 +0800

    [imp] index page sql optimization 1

--- tolmach/views.py
+++ tolmach/views.py
@@ -19,6 +19,8 @@ def index(request):
         first_name = request.user.first_name
         last_name = request.user.last_name
         projects = Project.objects.filter(manager=request.user.id).order_by('-last_modified')
+        for proj in projects:
+            proj.progress = proj.get_progress()
         usermeta, p = UserMeta.objects.get_or_create(user=request.user)
         ordered_stat, total_translated = utils.get_user_stat(request.user)
 
