ebd39197fc5aa6f7fd87a603bb7f994a9ba8a323
Dmitry Chumak <mega.venik@gmail.com>
2016-11-12 19:30:02 +0800

    [dbg] django-debug-toolbar added

--- tolmach/urls.py
+++ tolmach/urls.py
@@ -54,3 +54,9 @@ urlpatterns = patterns('',
 
     # temporarily added urls for developing purpuses
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
