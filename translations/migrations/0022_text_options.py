f4cd1756dc5e81ba51e60d6ef09336074d9ffb19
Dmitry Chumak <mega.venik@gmail.com>
2017-01-27 13:58:13 +0800

    [fix] Text options field default value is '{}' now

--- translations/migrations/0022_text_options.py
+++ translations/migrations/0022_text_options.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='text',
             name='options',
-            field=models.TextField(default=''),
+            field=models.TextField(default='{}'),
             preserve_default=False,
         ),
     ]
