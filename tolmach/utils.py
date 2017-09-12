49474486d7ba3d143226b30b4357afff27500b8d
Dmitry Chumak <mega.venik@gmail.com>
2017-09-12 20:26:16 +0800

    profile page new design

--- tolmach/utils.py
+++ tolmach/utils.py
@@ -19,25 +19,11 @@ def send_message(originator, addressee, message, type):
 
 
 def get_user_stat(user):
-    user_pairs = PairStats.objects.filter(user=user)
-    stat_langpairs = {}
+    user_pairs = PairStats.objects.filter(user=user).order_by('-fragments_translated')
+    print user_pairs
+    total_translated = sum([i.fragments_translated for i in user_pairs])
 
-    for i in user_pairs:
-        if not (i.source_lang, i.target_lang) in stat_langpairs.keys() and not (i.target_lang, i.source_lang) in stat_langpairs.keys():
-            stat_langpairs[(i.source_lang, i.target_lang)] = [i.fragments_translated, 0]
-        else:
-            try:
-                stat_langpairs[(i.source_lang, i.target_lang)][1] = i.fragments_translated
-            except:
-                stat_langpairs[(i.target_lang, i.source_lang)][1] = i.fragments_translated
-
-    total_translated = sum([sum(i) for i in stat_langpairs.values()])
-
-    for key, value in stat_langpairs.items():
-        stat_langpairs[key][0] = int(value[0]/(total_translated/100.0))
-        stat_langpairs[key][1] = int(value[1]/(total_translated/100.0))
-
-    ordered_stat = OrderedDict(sorted(stat_langpairs.items(), key=lambda t: sum(t[1]), reverse=True))
+    ordered_stat = user_pairs[:6]
 
     return ordered_stat, total_translated
 
