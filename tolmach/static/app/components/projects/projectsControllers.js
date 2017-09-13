3334a17308c1fc7e6557bd00c15bc0110486ceb7
Dmitry Chumak <mega.venik@gmail.com>
2017-09-13 16:55:23 +0800

    more profile page changes

--- tolmach/static/app/components/projects/projectsControllers.js
+++ tolmach/static/app/components/projects/projectsControllers.js
@@ -30,7 +30,7 @@
                 $scope.error = '';
                 var data = {
                     'name': $scope.name,
-                    'description': $scope.description,
+                    'description': $scope.description || " ",
                     'type': $scope.type
                 };
                 $scope.busy = true;
@@ -45,6 +45,10 @@
                     });
             };
 
+            $scope.addProjectAdvancedOptions = false;
+            $scope.addProjectAdvancedOptionsOpener = function () {
+                $scope.addProjectAdvancedOptions = !$scope.addProjectAdvancedOptions;
+            };
             $scope.cancel = function () {
                 $modalInstance.dismiss('cancel');
             };
