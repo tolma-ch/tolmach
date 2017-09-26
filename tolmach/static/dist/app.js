6410b020e99cbe5472c97ca3b1b67da382462c49
Dmitry Chumak <mega.venik@gmail.com>
2017-09-27 00:37:07 +0800

    dict show autofocus

--- tolmach/static/dist/app.js
+++ tolmach/static/dist/app.js
@@ -172,31 +172,26 @@
 
     var module = angular.module('dictDirectives', []);
 
-    module.directive('setFocus', function ($timeout) {
-        return function ($scope, $element, $attr) {
-            if ($attr.setFocus !== false) {
-                var timeout = 750; // wait 750 ms before evaluating
-                var focus = true;
-                if ($attr.setFocus) {
-                    // if we have focus criteria, then evaluate it against the scope (ex: set-focus="myValue === 1")
-                    focus = $scope.$eval($attr.setFocus);
-                }
-
-                // if we need to set focus to this element, then wait for the timeout and set focus
-                if (focus) {
-                    $timeout(function () {
-                        $element[0].focus();
-                    }, timeout);
-                }
+    module.directive('focusOn', ['$timeout', function($timeout) {
+        return {
+            restrict : 'A',
+            link : function($scope,$element,$attr) {
+                $scope.$watch($attr.focusOn,function(_focusVal) {
+                    $timeout(function() {
+                        _focusVal ? $element[0].focus() :
+                            $element[0].blur();
+                    });
+                });
             }
-        };
-    });
+        }
+    }]);
 }());;(function () {
     'use strict';
 
     angular.module('dictModule', [
         'dictServices',
-        'dictControllers'
+        'dictControllers',
+        'dictDirectives'
     ]);
 }());;(function () {
     'use strict';
