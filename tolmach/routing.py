7488792d564cd091e275110c74e7d516d4307a27
Dmitry Chumak <mega.venik@gmail.com>
2017-10-11 01:33:23 +0800

    translation objects' ws groups

--- tolmach/routing.py
+++ tolmach/routing.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
 from channels import route
 
 # This function will display all messages received in the console
@@ -7,7 +10,7 @@ def message_handler(message):
 
 channel_routing = [
     #route("websocket.receive", message_handler),  # we register our message handler
-    route('websocket.connect', 'tolmach.consumers.ws_connect'),
-    route('websocket.receive', 'tolmach.consumers.ws_message'),
-    route('websocket.disconnect', 'tolmach.consumers.ws_disconnect'),
+    route('websocket.connect', 'translations.consumers.ws_text_translation_connect', path=r'^/text/(?P<text_id>\d+)/(?P<target_lang>\w+)/$'),
+    route('websocket.receive', 'translations.consumers.ws_text_translation_message', path=r'^/text/(?P<text_id>\d+)/(?P<target_lang>\w+)/$'),
+    route('websocket.disconnect', 'translations.consumers.ws_text_translation_disconnect', path=r'^/text/(?P<text_id>\d+)/(?P<target_lang>\w+)/$'),
 ]
