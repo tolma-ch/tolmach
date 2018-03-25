b12eac92766c53920ba85d0ada7d8116f1537f59
Dmitry Chumak <mega.venik@gmail.com>
2018-03-25 17:51:38 +0800

    fullscreen check

--- tolmach/static/dist/app.js
+++ tolmach/static/dist/app.js
@@ -2971,6 +2971,39 @@
                     'width': textWidth + 'px'
                 };
             };
+            $scope.isFullscreenActive = false;
+            $scope.toggleFullscreen = function () {
+                if (screenfull.enabled) {
+                    if (!screenfull.isFullscreen) {
+                        screenfull.request();
+                        console.log('Am I fullscreen?', screenfull.isFullscreen ? 'Yes' : 'No');
+                        $scope.isFullscreenActive = true;
+                        console.log("checking scope:", $scope.isFullscreenActive);
+                    } else {
+                        screenfull.exit();
+                        $scope.isFullscreenActive = false;
+                    }
+                }
+            };
+            function fullscreenchange() {
+				var elem = screenfull.element;
+
+				$('#status').text('Is fullscreen: ' + screenfull.isFullscreen);
+
+				if (elem) {
+					$('#element').text('Element: ' + elem.localName + (elem.id ? '#' + elem.id : ''));
+				}
+
+				if (!screenfull.isFullscreen) {
+					$('#external-iframe').remove();
+					document.body.style.overflow = 'auto';
+				}
+			}
+
+			screenfull.on('change', fullscreenchange);
+
+			// Set the initial values
+			fullscreenchange();
             $scope.beginHelpPresentation = function () {
                 var event = $scope.$broadcast('helpPresentationStart');
                 if (event.customized) {
