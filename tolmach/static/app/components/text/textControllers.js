87f40bcddb042a8c7ac0f30dbeff1894b7b073a5
Dmitry Chumak <mega.venik@gmail.com>
2018-05-28 18:02:55 +0300

    fragment link fix

--- tolmach/static/app/components/text/textControllers.js
+++ tolmach/static/app/components/text/textControllers.js
@@ -13,7 +13,7 @@
 
             $scope.getHost = window['getHost'];
             $scope.isHttps = window['isHttps'];
-            $scope.baseHost = $scope.isHttps ? 'https' : 'http' + '://' + $scope.getHost;
+            $scope.baseHost = ($scope.isHttps ? 'https' : 'http') + '://' + $scope.getHost;
 
             $scope.keyLength = function (obj) {
                 return Object.keys(obj).length;
