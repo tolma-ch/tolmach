fa9052a28a83ee0243eaef3fa9b9b97b608bd4e9
Dmitry 🤔 Chumak <dchumak@itsumma.ru>
2018-07-02 18:03:54 +0800

    raven

--- tolmach/settings.py
+++ tolmach/settings.py
@@ -233,7 +233,8 @@ RAVEN_CONFIG = {
     'dsn': '***REMOVED***',
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
+    'release': raven.fetch_git_sha(BASE_DIR),
+    #'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
 }
 
 # A sample logging configuration. The only tangible logging
