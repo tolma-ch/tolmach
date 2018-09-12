b72d2cba4d3105d7d521bdd8c77efbf17c377738
Dmitry Chumak <mega.venik@gmail.com>
2018-09-12 23:38:02 +0800

    stats final

---
+++ stats/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+
+
+class PairStats(models.Model):
+    user = models.ForeignKey('auth.User', related_name="pair_stats_user", on_delete=models.deletion.CASCADE)
+    fragments_translated = models.IntegerField(default=0)
+    source_lang = models.ForeignKey('entries.Language', related_name='pair_stats_source_lang', on_delete=models.deletion.CASCADE)
+    target_lang = models.ForeignKey('entries.Language', related_name='pair_stats_target_lang', on_delete=models.deletion.CASCADE)
+
+
+class EntryStats(models.Model):
+    user = models.ForeignKey('auth.User', related_name="entry_stats_user", on_delete=models.deletion.SET_NULL, null=True)
+    date = models.IntegerField(default=0) # 20180905
+    action_type = models.CharField(max_length=15)
+    action_count = models.IntegerField(default=0)
+    characters_count = models.IntegerField(default=0)
+    project = models.ForeignKey('translations.Project', related_name='entry_stats_project', on_delete=models.deletion.SET_NULL, default=None, null=True)
+    last_modified = models.DateTimeField(default=timezone.now)
+
+
+class DictStats(models.Model):
+    user = models.ForeignKey('auth.User', related_name="dict_stats_user", on_delete=models.deletion.SET_NULL, null=True)
+    date = models.IntegerField(default=0) # 20180905
+    action_count = models.IntegerField(default=0)
+    source_lang = models.ForeignKey('entries.Language', related_name='dict_stats_source_lang', on_delete=models.deletion.CASCADE)
+    target_lang = models.ForeignKey('entries.Language', related_name='dict_stats_target_lang', on_delete=models.deletion.CASCADE)
+    last_modified = models.DateTimeField(default=timezone.now)
