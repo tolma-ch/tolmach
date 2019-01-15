cee4536f0b3b25e9bd56710e861aed7e70990ec2
Dmitry Chumak <mega.venik@gmail.com>
2019-01-15 20:57:27 +0800

    cursor to end when adding original/machines text by hotkey

--- tolmach/static/dist/app.js
+++ tolmach/static/dist/app.js
@@ -2267,10 +2267,20 @@
                         // hotkey for copying machine translation to textarea
                         if (typeof entry.yaMachines !== 'undefined') {
                             entry.suggestion = entry.yaMachines[0].text;
+                            var input = $('#entry-suggestion-' + entry.id);
+                            input.focus();
+                            setTimeout(function () {
+                                moveCursorToEnd(input[0]);
+                            }, 10);
                         }
                     } else if (code === 79) { // Alt - o
                         // hotkey for copying original text to textarea
-                        entry.suggestion = entry.body;
+                        entry.suggestion = entry.rawBody;
+                        var input = $('#entry-suggestion-' + entry.id);
+                        input.focus();
+                        setTimeout(function () {
+                            moveCursorToEnd(input[0]);
+                        }, 10);
                     } else if (code === 83) { // Alt - s
                         skipHotKey(entry);
                     }
