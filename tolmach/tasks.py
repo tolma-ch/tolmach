a11ee565ef1a8d33146fa37215a043ebdea68c1d
Dmitry Chumak <mega.venik@gmail.com>
2019-02-25 16:38:32 +0800

    more accurate project progress math

--- tolmach/tasks.py
+++ tolmach/tasks.py
@@ -53,8 +53,10 @@ def email_send(arguments):
 
 @cron(-10, -1, -1, -1, -1, target="worker")
 def update_projects_progress(arguments):
+    import math
     from datetime import datetime, timedelta
-    from translations.models import Project, Text, TextTranslation
+    from django.db.models import Q
+    from translations.models import Project, ProjectTranslation, Text, TextTranslation, TextEntry
 
     time_threshold = datetime.now() - timedelta(minutes=10)
     # if not arguments.get('full_update', False):
@@ -74,9 +76,25 @@ def update_projects_progress(arguments):
                 translated_progress += translation.get_progress()[1][0]
                 approved_progress += translation.get_progress()[1][1]
 
-        if not texts.count() == 0:
-            project_progress = [int(approved_progress / translations_num),
-                                int(translated_progress / translations_num) - int(approved_progress / translations_num)]
+        project_translations = ProjectTranslation.objects.filter(project=project).count()
+        entries_total = TextEntry.objects.filter(text__project=project, parent_entry=None).count() * project_translations
+        entries_disabled = TextEntry.objects.filter(text__project=project, parent_entry=None, is_disabled=True).count() * project_translations
+        entries_enabled = entries_total - entries_disabled
+        entries_translated = 0
+        for trans in TextTranslation.objects.filter(project_translation__project=project):
+            translated_entries_list = TextEntry.objects.filter(~Q(parent_entry=None),
+                                                               translation=trans).values('parent_entry').distinct()
+            translated_ids_list = [i['parent_entry'] for i in translated_entries_list]
+
+            entries_translated += TextEntry.objects.filter(id__in=translated_ids_list, is_disabled=False).count()
+        entries_approved = TextEntry.objects.filter(text__project=project, is_approved=True).count()
+
+        if not entries_total == 0:
+            percent_translated = int(math.ceil(entries_translated / (entries_enabled / 100.0))) if (
+                        entries_translated < entries_enabled) else 100
+            percent_approved = int(math.ceil(entries_approved / (entries_enabled / 100.0)))
+
+            project_progress = [percent_translated, percent_translated-percent_approved]
         else:
             project_progress = [0, 0]
 
