62a61e3d2467f634fc450c3b50513d2489622ee8
Dmitry Chumak <mega.venik@gmail.com>
2019-06-26 14:40:01 +0300

    fixing error when there're zero enabled entries in document

--- translations/models.py
+++ translations/models.py
@@ -325,7 +325,7 @@ class TextTranslation(models.Model):
                 entries_approved = TextEntry.objects.filter(text=self.text, translation=self, is_approved=True).count()
                 cache.set('%d_translation_progress' % self.id, [entries_total, entries_translated, entries_approved, entries_disabled], 60*10)
 
-            if not entries_total == 0:
+            if not entries_total == 0 and not entries_enabled == 0:
                 percent_translated = int(math.ceil(entries_translated/( entries_enabled /100.0))) if (entries_translated < entries_enabled) else 100
                 percent_approved = int(math.ceil(entries_approved/( entries_enabled /100.0)))
 
