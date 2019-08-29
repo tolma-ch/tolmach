42bf40839544720e359d2fafb7f7b48cc1fdc027
Dmitry Chumak <mega.venik@gmail.com>
2019-08-29 11:48:01 +0300

    [fix] Shift-Shift search hotkey trigger changed to keyup

--- tolmach/static/app/shared/main/mainControllers.js
+++ tolmach/static/app/shared/main/mainControllers.js
@@ -208,6 +208,9 @@
             $scope.globalKeydown = function (event) {
                 $scope.$broadcast('GlobalKeydown', event);
             };
+            $scope.globalKeyup = function (event) {
+                $scope.$broadcast('GlobalKeyup', event);
+            };
             $scope.bodyClick = function (event) {
                 $scope.$broadcast('GlobalClick', event);
             };
@@ -224,13 +227,13 @@
             };
 
             $scope.lastKeysPressed = [];
-            $scope.$on('GlobalKeydown', function (e, event) {
+            $scope.$on('GlobalKeyup', function (e, event) {
                 console.log($scope.lastKeysPressed);
                 var code = event.keyCode ? event.keyCode : event.which;
                 $scope.lastKeysPressed.push({"time": Date.now(), "code": code});
                 $scope.lastKeysPressed = $scope.lastKeysPressed.slice(-2);
                 if ( ($scope.lastKeysPressed[0].code === 16 && $scope.lastKeysPressed[1].code === 16) &&
-                   ($scope.lastKeysPressed[1].time - $scope.lastKeysPressed[0].time < 200) ) { // Double-shift press
+                   ($scope.lastKeysPressed[1].time - $scope.lastKeysPressed[0].time < 300) ) { // Double-shift press
                     $scope.lastKeysPressed = [];
                     $scope.showSearch = true;
                     setTimeout(function () {
