2a7fb45a20b4e415a7f3a4c92e44df75ed08966c
Alexei Shabalin <alshabalin@yandex.ru>
2020-02-25 18:20:39 +0800

    popover appended to body

--- tolmach/static/dist/app.js
+++ tolmach/static/dist/app.js
@@ -2834,7 +2834,7 @@
                     $timeout(function() {
                         entry.popoverIsOpen = true;
                         $timeout(function() {
-                            var el = angular.element('#entry-'+entry.idInText+' + .popover')[0];
+                            var el = angular.element('.active-entry-popover')[0];
                             angular.element(el).css({
                                 marginLeft: $event.offsetX,
                                 marginTop: $event.offsetY - 16,
