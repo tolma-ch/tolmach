d9eb4e3687cfb5b6f6b93db710f678e41c14ab4a
Dmitry Chumak <mega.venik@gmail.com>
2020-04-11 11:28:40 +0300

    fixed cron tasks cleaning process

--- translations/apps.py
+++ translations/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
+from django.db.utils import ProgrammingError
 
 class TranslationsConfig(AppConfig):
     name = 'translations'
@@ -8,6 +9,9 @@ class TranslationsConfig(AppConfig):
     def ready(self):
         import translations.signals  # noqa
         from tolmach import tasks
-        from background_task.models import Task
-        Task.objects.filter(task_name="tolmach.tasks.update_projects_progress").delete()
+        try:
+            from background_task.models import Task
+            Task.objects.filter(task_name="tolmach.tasks.update_projects_progress").delete()
+        except ProgrammingError:
+            pass
         tasks.update_projects_progress(repeat=60*10)
